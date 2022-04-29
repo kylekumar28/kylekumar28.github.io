df96f36147ede01b8333b4f17d163b10915dc24b
Kyle Kumar <42920083+kylekumar28@users.noreply.github.com>
2022-04-29 12:55:05 +0100

    cocktail app

---
+++ react/cocktail-recipe-app/precache-manifest.fbd03f086503fa6603034327891597a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5780dc5aa6c145ac2d37f339323267",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e46d9373a3e95ac33ab",
+    "url": "/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e46d9373a3e95ac33ab",
+    "url": "/static/js/main.e8b5c8bf.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
