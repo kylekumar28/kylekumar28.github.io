7e8f56a88f130b587e88c33e07aa89ee693728c2
Kyle Kumar <42920083+kylekumar28@users.noreply.github.com>
2022-05-19 10:16:40 +0100

    added cocktail

---
+++ react/cocktail-recipe-app/precache-manifest.5ec61e000a09b9a23238341a2f381ea4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5a2c8b3afa192bfce9150999764e48",
+    "url": "/cocktail-recipe-app/index.html"
+  },
+  {
+    "revision": "e1bcc97ba1665242c51d",
+    "url": "/cocktail-recipe-app/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "68e9d93a1dfba5322ade",
+    "url": "/cocktail-recipe-app/static/js/2.e42185e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktail-recipe-app/static/js/2.e42185e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1bcc97ba1665242c51d",
+    "url": "/cocktail-recipe-app/static/js/main.b9f6dcc3.chunk.js"
+  },
+  {
+    "revision": "d5afc75e27205989e03b",
+    "url": "/cocktail-recipe-app/static/js/runtime-main.cf1abd35.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/cocktail-recipe-app/static/media/logo.9a3d2645.svg"
+  }
+]);
--- react/cocktail-recipe-app/service-worker.js
+++ react/cocktail-recipe-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbd03f086503fa6603034327891597a7.js"
+  "/cocktail-recipe-app/precache-manifest.5ec61e000a09b9a23238341a2f381ea4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cocktail-recipe-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
